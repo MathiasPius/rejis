7128bd89202f336693ef8cca4fc40a2cec27c8cb
Mathias Pius <contact@pius.io>
2023-03-11 18:55:17 +0100

    Gate tests behind derive and macro features.

--- rejis/tests/filtering.rs
+++ rejis/tests/filtering.rs
@@ -1,179 +1,182 @@
-use rejis::{
-    filter::And,
-    filter::Operator::{Equal, Like, NotEqual},
-    Table,
-};
+#[cfg(feature = "derive")]
+mod filtering {
+    use rejis::{
+        filter::And,
+        filter::Operator::{Equal, Like, NotEqual},
+        Table,
+    };
 
-use crate::testutils::User;
+    use testutils::User;
 
-mod testutils {
-    use rejis::{Database, Queryable, Table};
-    use rusqlite::Connection;
-    use serde::{Deserialize, Serialize};
+    mod testutils {
+        use rejis::{Database, Queryable, Table};
+        use rusqlite::Connection;
+        use serde::{Deserialize, Serialize};
 
-    #[derive(Queryable, Serialize, Deserialize, Debug, Clone)]
-    pub struct Pet {
-        pub name: String,
-    }
+        #[derive(Queryable, Serialize, Deserialize, Debug, Clone)]
+        pub struct Pet {
+            pub name: String,
+        }
 
-    #[derive(Queryable, Table, Serialize, Deserialize, Debug, Clone)]
-    pub struct User {
-        pub first_name: String,
-        pub last_name: String,
-        pub age: u8,
-        pub pets: Vec<Pet>,
-    }
+        #[derive(Queryable, Table, Serialize, Deserialize, Debug, Clone)]
+        pub struct User {
+            pub first_name: String,
+            pub last_name: String,
+            pub age: u8,
+            pub pets: Vec<Pet>,
+        }
 
-    /// Utility function providing a database pre-seeded with a number of different users
-    pub fn user_database() -> Database {
-        // Open an in-memory database, create the table and populate it with
-        // three users.
-        let db = Database::new(Connection::open_in_memory().unwrap());
-        db.create_table::<User>().unwrap();
+        /// Utility function providing a database pre-seeded with a number of different users
+        pub fn user_database() -> Database {
+            // Open an in-memory database, create the table and populate it with
+            // three users.
+            let db = Database::new(Connection::open_in_memory().unwrap());
+            db.create_table::<User>().unwrap();
 
-        // John Smith
-        db.insert(User {
-            first_name: String::from("John"),
-            last_name: String::from("Smith"),
-            age: 32,
-            pets: vec![
-                Pet {
-                    name: String::from("Garfield"),
-                },
-                Pet {
-                    name: String::from("Lucky"),
-                },
-            ],
-        });
+            // John Smith
+            db.insert(User {
+                first_name: String::from("John"),
+                last_name: String::from("Smith"),
+                age: 32,
+                pets: vec![
+                    Pet {
+                        name: String::from("Garfield"),
+                    },
+                    Pet {
+                        name: String::from("Lucky"),
+                    },
+                ],
+            });
 
-        // Jane Smith
-        db.insert(User {
-            first_name: String::from("Jane"),
-            last_name: String::from("Smith"),
-            age: 35,
-            pets: vec![
-                Pet {
+            // Jane Smith
+            db.insert(User {
+                first_name: String::from("Jane"),
+                last_name: String::from("Smith"),
+                age: 35,
+                pets: vec![
+                    Pet {
+                        name: String::from("Jimmy"),
+                    },
+                    Pet {
+                        name: String::from("Jimmy"),
+                    },
+                ],
+            });
+
+            // Thomas Anderson
+            db.insert(User {
+                first_name: String::from("Thomas"),
+                last_name: String::from("Anderson"),
+                age: 24,
+                pets: vec![],
+            });
+
+            // John Anderson
+            db.insert(User {
+                first_name: String::from("John"),
+                last_name: String::from("Anderson"),
+                age: 48,
+                pets: vec![Pet {
                     name: String::from("Jimmy"),
-                },
-                Pet {
-                    name: String::from("Jimmy"),
-                },
-            ],
-        });
+                }],
+            });
 
-        // Thomas Anderson
-        db.insert(User {
-            first_name: String::from("Thomas"),
-            last_name: String::from("Anderson"),
-            age: 24,
-            pets: vec![],
-        });
+            // Richard LaFleur
+            db.insert(User {
+                first_name: String::from("Richard"),
+                last_name: String::from("LaFleur"),
+                age: 36,
+                pets: vec![],
+            });
 
-        // John Anderson
-        db.insert(User {
-            first_name: String::from("John"),
-            last_name: String::from("Anderson"),
-            age: 48,
-            pets: vec![Pet {
-                name: String::from("Jimmy"),
-            }],
-        });
-
-        // Richard LaFleur
-        db.insert(User {
-            first_name: String::from("Richard"),
-            last_name: String::from("LaFleur"),
-            age: 36,
-            pets: vec![],
-        });
-
-        db
+            db
+        }
     }
-}
 
-#[test]
-fn single_property_equality() {
-    let db = testutils::user_database();
+    #[test]
+    fn single_property_equality() {
+        let db = testutils::user_database();
 
-    let johns = db.get(User::query().first_name.cmp(Equal, "John")).unwrap();
+        let johns = db.get(User::query().first_name.cmp(Equal, "John")).unwrap();
 
-    assert_eq!(johns.len(), 2);
-    assert!(johns.iter().all(|john| john.first_name == "John"));
-}
+        assert_eq!(johns.len(), 2);
+        assert!(johns.iter().all(|john| john.first_name == "John"));
+    }
 
-#[test]
-fn single_property_inequality() {
-    let db = testutils::user_database();
+    #[test]
+    fn single_property_inequality() {
+        let db = testutils::user_database();
 
-    let non_smiths = db
-        .get(User::query().last_name.cmp(NotEqual, "Smith"))
-        .unwrap();
+        let non_smiths = db
+            .get(User::query().last_name.cmp(NotEqual, "Smith"))
+            .unwrap();
 
-    assert_eq!(non_smiths.len(), 3);
-    assert!(non_smiths
-        .iter()
-        .all(|non_smith| non_smith.last_name != "Smith"));
-}
+        assert_eq!(non_smiths.len(), 3);
+        assert!(non_smiths
+            .iter()
+            .all(|non_smith| non_smith.last_name != "Smith"));
+    }
 
-#[test]
-fn multi_property_equality() {
-    let db = testutils::user_database();
+    #[test]
+    fn multi_property_equality() {
+        let db = testutils::user_database();
 
-    // Find all John Smiths
-    let john_smith = db
-        .get(And((
-            User::query().first_name.cmp(Equal, "John"),
-            User::query().last_name.cmp(Equal, "Smith"),
-        )))
-        .unwrap();
+        // Find all John Smiths
+        let john_smith = db
+            .get(And((
+                User::query().first_name.cmp(Equal, "John"),
+                User::query().last_name.cmp(Equal, "Smith"),
+            )))
+            .unwrap();
 
-    assert_eq!(john_smith.len(), 1);
-    assert_eq!(john_smith[0].first_name, "John");
-    assert_eq!(john_smith[0].last_name, "Smith");
-    assert_eq!(john_smith[0].age, 32);
-}
+        assert_eq!(john_smith.len(), 1);
+        assert_eq!(john_smith[0].first_name, "John");
+        assert_eq!(john_smith[0].last_name, "Smith");
+        assert_eq!(john_smith[0].age, 32);
+    }
 
-#[test]
-fn multi_property_inequality() {
-    let db = testutils::user_database();
+    #[test]
+    fn multi_property_inequality() {
+        let db = testutils::user_database();
 
-    // Find all non-Smith Johns
-    let john_smith = db
-        .get(And((
-            User::query().first_name.cmp(Equal, "John"),
-            User::query().last_name.cmp(NotEqual, "Smith"),
-        )))
-        .unwrap();
+        // Find all non-Smith Johns
+        let john_smith = db
+            .get(And((
+                User::query().first_name.cmp(Equal, "John"),
+                User::query().last_name.cmp(NotEqual, "Smith"),
+            )))
+            .unwrap();
 
-    println!("{john_smith:?}");
+        println!("{john_smith:?}");
 
-    assert_eq!(john_smith.len(), 1);
-    assert_eq!(john_smith[0].first_name, "John");
-    assert_eq!(john_smith[0].last_name, "Anderson");
-    assert_eq!(john_smith[0].age, 48);
-}
+        assert_eq!(john_smith.len(), 1);
+        assert_eq!(john_smith[0].first_name, "John");
+        assert_eq!(john_smith[0].last_name, "Anderson");
+        assert_eq!(john_smith[0].age, 48);
+    }
 
-#[test]
-fn like_matching() {
-    let db = testutils::user_database();
+    #[test]
+    fn like_matching() {
+        let db = testutils::user_database();
 
-    let jays = db.get(User::query().first_name.cmp(Like, "J%")).unwrap();
+        let jays = db.get(User::query().first_name.cmp(Like, "J%")).unwrap();
 
-    // Should yield John Smith, Jane Smith and John Anderson
-    assert_eq!(jays.len(), 3);
-}
+        // Should yield John Smith, Jane Smith and John Anderson
+        assert_eq!(jays.len(), 3);
+    }
 
-#[test]
-fn array_matching() {
-    let db = testutils::user_database();
+    #[test]
+    fn array_matching() {
+        let db = testutils::user_database();
 
-    let garfield_owners = db
-        .get(
-            User::query()
-                .pets
-                .any(|query| query.name.clone(), Like, "Jimmy"),
-        )
-        .unwrap();
+        let garfield_owners = db
+            .get(
+                User::query()
+                    .pets
+                    .any(|query| query.name.clone(), Like, "Jimmy"),
+            )
+            .unwrap();
 
-    println!("{:#?}", garfield_owners);
+        println!("{:#?}", garfield_owners);
+    }
 }
--- rejis/tests/macro_query.rs
+++ rejis/tests/macro_query.rs
@@ -1,245 +1,248 @@
-use rejis::Q;
+#[cfg(all(feature = "macros", feature = "derive"))]
+mod macros {
+    use rejis::Q;
 
-use rejis::Table;
-use testutils::user_database;
-use testutils::User;
+    use rejis::Table;
+    use testutils::user_database;
+    use testutils::User;
 
-mod testutils {
-    use rejis::{Database, Queryable, Table};
-    use rusqlite::Connection;
-    use serde::{Deserialize, Serialize};
+    mod testutils {
+        use rejis::{Database, Queryable, Table};
+        use rusqlite::Connection;
+        use serde::{Deserialize, Serialize};
 
-    #[derive(Queryable, Serialize, Deserialize, Debug, Clone)]
-    pub struct Pet {
-        pub name: String,
-    }
+        #[derive(Queryable, Serialize, Deserialize, Debug, Clone)]
+        pub struct Pet {
+            pub name: String,
+        }
 
-    #[derive(Queryable, Table, Serialize, Deserialize, Debug, Clone)]
-    pub struct User {
-        pub first_name: String,
-        pub last_name: String,
-        pub age: u8,
-        pub pets: Vec<Pet>,
-    }
+        #[derive(Queryable, Table, Serialize, Deserialize, Debug, Clone)]
+        pub struct User {
+            pub first_name: String,
+            pub last_name: String,
+            pub age: u8,
+            pub pets: Vec<Pet>,
+        }
 
-    /// Utility function providing a database pre-seeded with a number of different users
-    pub fn user_database() -> Database {
-        // Open an in-memory database, create the table and populate it with
-        // three users.
-        let db = Database::new(Connection::open_in_memory().unwrap());
-        db.create_table::<User>().unwrap();
+        /// Utility function providing a database pre-seeded with a number of different users
+        pub fn user_database() -> Database {
+            // Open an in-memory database, create the table and populate it with
+            // three users.
+            let db = Database::new(Connection::open_in_memory().unwrap());
+            db.create_table::<User>().unwrap();
 
-        // John Smith
-        db.insert(User {
-            first_name: String::from("John"),
-            last_name: String::from("Smith"),
-            age: 32,
-            pets: vec![
-                Pet {
-                    name: String::from("Garfield"),
-                },
-                Pet {
-                    name: String::from("Lucky"),
-                },
-            ],
-        });
+            // John Smith
+            db.insert(User {
+                first_name: String::from("John"),
+                last_name: String::from("Smith"),
+                age: 32,
+                pets: vec![
+                    Pet {
+                        name: String::from("Garfield"),
+                    },
+                    Pet {
+                        name: String::from("Lucky"),
+                    },
+                ],
+            });
 
-        // Jane Smith
-        db.insert(User {
-            first_name: String::from("Jane"),
-            last_name: String::from("Smith"),
-            age: 35,
-            pets: vec![
-                Pet {
+            // Jane Smith
+            db.insert(User {
+                first_name: String::from("Jane"),
+                last_name: String::from("Smith"),
+                age: 35,
+                pets: vec![
+                    Pet {
+                        name: String::from("Jimmy"),
+                    },
+                    Pet {
+                        name: String::from("Jimmy"),
+                    },
+                ],
+            });
+
+            // Thomas Anderson
+            db.insert(User {
+                first_name: String::from("Thomas"),
+                last_name: String::from("Anderson"),
+                age: 24,
+                pets: vec![],
+            });
+
+            // John Anderson
+            db.insert(User {
+                first_name: String::from("John"),
+                last_name: String::from("Anderson"),
+                age: 48,
+                pets: vec![Pet {
                     name: String::from("Jimmy"),
-                },
-                Pet {
-                    name: String::from("Jimmy"),
-                },
-            ],
-        });
+                }],
+            });
 
-        // Thomas Anderson
-        db.insert(User {
-            first_name: String::from("Thomas"),
-            last_name: String::from("Anderson"),
-            age: 24,
-            pets: vec![],
-        });
+            // Richard LaFleur
+            db.insert(User {
+                first_name: String::from("Richard"),
+                last_name: String::from("LaFleur"),
+                age: 36,
+                pets: vec![],
+            });
 
-        // John Anderson
-        db.insert(User {
-            first_name: String::from("John"),
-            last_name: String::from("Anderson"),
-            age: 48,
-            pets: vec![Pet {
-                name: String::from("Jimmy"),
-            }],
-        });
-
-        // Richard LaFleur
-        db.insert(User {
-            first_name: String::from("Richard"),
-            last_name: String::from("LaFleur"),
-            age: 36,
-            pets: vec![],
-        });
-
-        db
+            db
+        }
     }
-}
 
-#[test]
-fn simple_filtering_dsl() {
-    let db = user_database();
+    #[test]
+    fn simple_filtering_dsl() {
+        let db = user_database();
 
-    let johns = db
-        .get(Q! {
-            User.first_name == "John"
-        })
-        .unwrap();
+        let johns = db
+            .get(Q! {
+                User.first_name == "John"
+            })
+            .unwrap();
 
-    println!("{:#?}", johns);
-    assert_eq!(johns.len(), 2);
-}
+        println!("{:#?}", johns);
+        assert_eq!(johns.len(), 2);
+    }
 
-#[test]
-fn multi_filtering_dsl() {
-    let db = user_database();
+    #[test]
+    fn multi_filtering_dsl() {
+        let db = user_database();
 
-    let johns = db
-        .get(Q! {
-            (User.first_name == "John") && (User.last_name != "Smith")
-        })
-        .unwrap();
+        let johns = db
+            .get(Q! {
+                (User.first_name == "John") && (User.last_name != "Smith")
+            })
+            .unwrap();
 
-    println!("{:#?}", johns);
-    assert_eq!(johns.len(), 1);
-    assert_eq!(johns[0].last_name, "Anderson");
-}
+        println!("{:#?}", johns);
+        assert_eq!(johns.len(), 1);
+        assert_eq!(johns[0].last_name, "Anderson");
+    }
 
-#[test]
-fn multi_filtering_or_dsl() {
-    let db = user_database();
+    #[test]
+    fn multi_filtering_or_dsl() {
+        let db = user_database();
 
-    let query = Q! {
-        (
-            (User.first_name == "John") && (User.last_name == "Smith")
-        ) ||
-        (
-            (User.first_name == "Thomas") && (User.last_name == "Anderson")
-        )
-    };
+        let query = Q! {
+            (
+                (User.first_name == "John") && (User.last_name == "Smith")
+            ) ||
+            (
+                (User.first_name == "Thomas") && (User.last_name == "Anderson")
+            )
+        };
 
-    let johns = db.get(query).unwrap();
+        let johns = db.get(query).unwrap();
 
-    println!("{:#?}", johns);
-    assert_eq!(johns.len(), 2);
-}
+        println!("{:#?}", johns);
+        assert_eq!(johns.len(), 2);
+    }
 
-#[test]
-fn expr_query() {
-    let db = user_database();
+    #[test]
+    fn expr_query() {
+        let db = user_database();
 
-    let first_name = vec!["John"];
+        let first_name = vec!["John"];
 
-    let query = Q! {
-        User.first_name == &first_name[0]
-    };
+        let query = Q! {
+            User.first_name == &first_name[0]
+        };
 
-    let johns = db.get(query).unwrap();
+        let johns = db.get(query).unwrap();
 
-    println!("{:#?}", johns);
-    assert_eq!(johns.len(), 2);
-}
+        println!("{:#?}", johns);
+        assert_eq!(johns.len(), 2);
+    }
 
-#[test]
-fn ident_query() {
-    let db = user_database();
+    #[test]
+    fn ident_query() {
+        let db = user_database();
 
-    let first_name = "John";
+        let first_name = "John";
 
-    let query = Q! {
-        User.first_name == first_name
-    };
+        let query = Q! {
+            User.first_name == first_name
+        };
 
-    let johns = db.get(query).unwrap();
+        let johns = db.get(query).unwrap();
 
-    println!("{:#?}", johns);
-    assert_eq!(johns.len(), 2);
-}
+        println!("{:#?}", johns);
+        assert_eq!(johns.len(), 2);
+    }
 
-#[test]
-fn any_query_literal() {
-    let db = user_database();
+    #[test]
+    fn any_query_literal() {
+        let db = user_database();
 
-    let garfields = Q! {
-        User.pets[..].name == "Garfield"
-    };
+        let garfields = Q! {
+            User.pets[..].name == "Garfield"
+        };
 
-    let garfield_owners = db.get(garfields).unwrap();
+        let garfield_owners = db.get(garfields).unwrap();
 
-    println!("{:#?}", garfield_owners);
-    assert_eq!(garfield_owners.len(), 1);
-}
+        println!("{:#?}", garfield_owners);
+        assert_eq!(garfield_owners.len(), 1);
+    }
 
-#[test]
-fn any_query_ident() {
-    let db = user_database();
+    #[test]
+    fn any_query_ident() {
+        let db = user_database();
 
-    let name = "Garfield";
+        let name = "Garfield";
 
-    let garfields = Q! {
-        User.pets[..].name == name
-    };
+        let garfields = Q! {
+            User.pets[..].name == name
+        };
 
-    let garfield_owners = db.get(garfields).unwrap();
+        let garfield_owners = db.get(garfields).unwrap();
 
-    println!("{:#?}", garfield_owners);
-    assert_eq!(garfield_owners.len(), 1);
-}
+        println!("{:#?}", garfield_owners);
+        assert_eq!(garfield_owners.len(), 1);
+    }
 
-#[test]
-fn any_query_complex() {
-    let db = user_database();
+    #[test]
+    fn any_query_complex() {
+        let db = user_database();
 
-    let name = vec!["Garfield"];
+        let name = vec!["Garfield"];
 
-    let garfields = Q! {
-        User.pets[..].name == &name[0]
-    };
+        let garfields = Q! {
+            User.pets[..].name == &name[0]
+        };
 
-    let garfield_owners = db.get(garfields).unwrap();
+        let garfield_owners = db.get(garfields).unwrap();
 
-    println!("{:#?}", garfield_owners);
-    assert_eq!(garfield_owners.len(), 1);
-}
+        println!("{:#?}", garfield_owners);
+        assert_eq!(garfield_owners.len(), 1);
+    }
 
-#[test]
-fn any_query_multiples() {
-    let db = user_database();
+    #[test]
+    fn any_query_multiples() {
+        let db = user_database();
 
-    let jimmies = Q! {
-        User.pets[..].name == "Jimmy"
-    };
+        let jimmies = Q! {
+            User.pets[..].name == "Jimmy"
+        };
 
-    let jimmy_owners = db.get(jimmies).unwrap();
+        let jimmy_owners = db.get(jimmies).unwrap();
 
-    println!("{:#?}", jimmy_owners);
-    assert_eq!(jimmy_owners.len(), 2);
-}
+        println!("{:#?}", jimmy_owners);
+        assert_eq!(jimmy_owners.len(), 2);
+    }
 
-#[test]
-fn complex_any_query() {
-    let db = user_database();
+    #[test]
+    fn complex_any_query() {
+        let db = user_database();
 
-    let jane = Q! {
-        (User.pets[..].name == "Jimmy") && (User.last_name == "Smith")
-    };
+        let jane = Q! {
+            (User.pets[..].name == "Jimmy") && (User.last_name == "Smith")
+        };
 
-    let jane = db.get(jane).unwrap();
+        let jane = db.get(jane).unwrap();
 
-    println!("{:#?}", jane);
-    assert_eq!(jane.len(), 1);
-    println!("{:#?}", jane[0]);
+        println!("{:#?}", jane);
+        assert_eq!(jane.len(), 1);
+        println!("{:#?}", jane[0]);
+    }
 }
